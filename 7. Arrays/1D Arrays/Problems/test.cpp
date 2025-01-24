6c1cefcfba78d972a02f262f6186e7c926d5e611
Sagnik Mukherjee <83669035+itssagnikmukherjee@users.noreply.github.com>
2025-01-24 16:57:48 +0530

    new array problem added

---
+++ 7. Arrays/1D Arrays/Problems/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] ={1,-1,3,-2,4,-5,2,-3};
+    int size = sizeof(arr)/sizeof(int);
+    int i = 0;
+    int j = size-1;
+    while(i<=j){
+        if(arr[i]>0 && arr[j]<0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+        if(arr[j]>0) j--;
+        if(arr[i]<0) i++;
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
